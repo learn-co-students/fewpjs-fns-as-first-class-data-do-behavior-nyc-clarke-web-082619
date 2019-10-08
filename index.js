33c0ab39fbc3e4b9956f3759dd4f3f47494313fb
Evan Krone <evankrone@Evans-MBP-2.fios-router.home>
2019-10-07 22:48:35 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = time.split(":").shift()
+  if (hour < 12){
+    return "Good Morning";
+    } else if (hour > 17){
+      return "Good Evening";
+    } else  if (hour > 12 && hour < 17){
+      return "Good Afternoon";
+    }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(newContent) {
+  document.getElementById('greeting').innerText = newContent;
+}
